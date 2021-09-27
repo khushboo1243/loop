e867c337888eabd135bae1cdb18c7d464b47808e
khushboo1243 <91380681+khushboo1243@users.noreply.github.com>
2021-09-27 09:57:46 +0530

    Add files via upload

---
+++ +ve and -ve no print.py
@@ -0,0 +1,40 @@
+# Loop ka use kar ke yeh pattern print karo. 1, -2, 3, -4, 5, 
+
+
+# i=1
+# while i<=10:
+#     if -1*2==0:
+#         print(i)
+#     else:
+#         print(-i)
+#     i=i+1
+    
+
+
+# i=1
+# while i<=10:
+#     if i%2==0:
+#         print(-i)
+#     else:
+#         print(i)
+#     i=i+1
+
+# a=5
+# b="7"
+
+# c=int(b)
+# print(c+a)
+
+# print(str(a)+b)
+# print("b"+a)
+# print(int(a)+"b")
+
+
+# c=int(b)
+# print(c)
+
+
+
+name="khusboo"
+a=name.strip()
+print(a)
